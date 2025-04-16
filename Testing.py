a044349545057b334b5c28b5922f435ee566ce29
JalexR <Johnalexras@gmail.com>
2025-04-16 12:56:32 -0500

    Changed to using outside chess implementation

--- Testing.py
+++ Testing.py
@@ -6,18 +6,25 @@ import Agents
 window_thread = threading.Thread(target=Visualize.create_window)
 window_thread.start()
 
-result = [3]
-board = chess.Board()
-for i in range(0,1000):
-    Visualize.moves_que.put(board.board_fen())
+result = [0 for i in range(3)]
+for n in range(20):
+    board = chess.Board()
     move = Agents.RandomAgent(board)
-    if move == None:
-        outcome = board.outcome()
-        if 
-        result.append()
-        break
-    else:
+    
+    while move != None:
+        Visualize.moves_que.put(board.board_fen())
         board.push(move)
+        move = Agents.RandomAgent(board)
+
+    outcome = board.outcome()
+    if outcome == chess.WHITE:
+        result[0] += 1
+    if outcome == chess.BLACK:
+        result[2] += 1
+    else:
+        result[1] += 1
+    result.append()
+    board.reset()
 
 print(result)
 
